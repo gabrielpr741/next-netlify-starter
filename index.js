4b36f69e1f1e3859de87f7740950f9fcddd4f6a1
gabrielpr741 <125768084+gabrielpr741@users.noreply.github.com>
2023-02-23 00:09:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Sac-Hotline!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Suporte SAC-HL" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Tudo o que você precisa em um só lugar <code>tahto.netlify.app</code>
         </p>
       </main>
 
