22310a95d6f960c631bf86498939d909aceca6a6
gabrielpr741 <125768084+gabrielpr741@users.noreply.github.com>
2023-02-25 11:52:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Sac-Hotline!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="https://media.glassdoor.com/sqll/5705884/tahto-squarelogo-1646295180355.png"/>
       </Head>
 
       <main>
-        <Header title="Suporte SAC-HL" />
+        <Header title="Suporte SAC-HOTLINE" />
         <p className="description">
           Tudo o que você precisa em um só lugar <code>tahto.netlify.app</code>
         </p>
