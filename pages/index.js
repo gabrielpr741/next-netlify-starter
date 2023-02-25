43a6a135252f6a46edd4c3892d12c914bb5ac7c8
gabrielpr741 <125768084+gabrielpr741@users.noreply.github.com>
2023-02-25 12:00:37 -0300

    Favicon

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>SAC Hotline</title>
-    <link rel="icon" type="image/png" href="https://media.glassdoor.com/sqll/5705884/tahto-squarelogo-1646295180355.png"/>
+        <title>Sac-Hotline!</title>
+        <link rel="icon" type="image/png" href="https://media.glassdoor.com/sqll/5705884/tahto-squarelogo-1646295180355.png"/>
       </Head>
 
       <main>
+        <Header title="Suporte SAC-HOTLINE" />
+        <p className="description">
+          Tudo o que você precisa em um só lugar <code>tahto.netlify.app</code>
+        </p>
       </main>
 
       <Footer />
