eff3974cd4d7f0ee5f94f11f360acf801d99dd42
gabrielpr741 <125768084+gabrielpr741@users.noreply.github.com>
2023-02-25 12:19:05 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>SAC~Hotline</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Suporte SAC-HL" />
+        <p className="description">
+          Tudo o que você precisa em um só lugar <code>tahto.netlify.app</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
