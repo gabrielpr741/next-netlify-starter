1832fd91e275d3ac1972c7a563dde7e86f5762d7
gabrielpr741 <125768084+gabrielpr741@users.noreply.github.com>
2023-02-25 11:46:41 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,10 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>SAC Hotline</title>
-        <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="https://media.glassdoor.com/sqll/5705884/tahto-squarelogo-1646295180355.png"/>
       </Head>
 
       <main>
-        <Header title="Suporte SAC-HL" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
